28bc59ac2accaba7bc884c72e67320ddee916873
Andrew Thornton-Smith <tsandrew@aphelion.bi>
2014-01-20 19:19:55 +0200

    Troubleshooting by Mark

---
+++ LightSwitch/AzureVM/AzureVM/AzureVM.HTMLClient/UserCode/vmControl.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.vmControl.created = function (entity) {
+    // Write code here.
+    entity.RequestedBy = "NA";
+};
---
+++ LightSwitch/AzureVM/AzureVM/AzureVM.Server/DataSources/AzureVMData/_AzureVMDataService.lsml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Security.Server;
+namespace LightSwitchApplication
+{
+    public partial class AzureVMDataService
+    {
+        //private string strUser = "";
+        //public string UserName
+        //{
+        //    get
+        //    {
+        //        if (strUser == "")
+        //        {
+        //            switch (Application.User.Name)
+        //            {
+        //                case "TestUser":
+        //                    strUser = Environment.UserName;
+        //                    break;
+        //                case "":
+        //                    strUser = Environment.UserName;
+        //                    break;
+        //                default:
+        //                    strUser = Application.User.Name.Split('|')[2];
+        //                    break;
+        //            }
+
+        //            if (strUser == "")
+        //            {
+        //                strUser = Application.User.PersonId;
+        //            }
+
+        //        }
+        //        return strUser;
+        //    }
+        //}
+
+
+        partial void vmControls_Inserting(vmControl entity)
+        {
+            entity.RequestedBy = this.Application.User.PersonId;// Environment.UserName;
+        }
+
+        partial void vmControls_Updating(vmControl entity)
+        {
+            entity.RequestedBy = this.Application.User.PersonId;// Environment.UserName;
+        }
+    }
+}
