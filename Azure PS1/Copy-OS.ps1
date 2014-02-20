edd9ee56110501273a4b577f4186833b9a785a7a
Andrew Thornton-Smith <andrewthorntonsmith@aphelion.bi>
2014-02-20 23:15:55 +0200

    Updates to files

--- Azure PS1/Copy-OS.ps1
+++ Azure PS1/Copy-OS.ps1
@@ -1,14 +1,14 @@
 
 Select-AzureSubscription "Visual Studio Premium with MSDN" 
  
-### Source VHD (West US) - authenticated container ###
-$srcUri = "http://devsps01.blob.core.windows.net/base-os2012-ad/devsps01-Drive01.vhd" 
+### Source VHD  - authenticated container ###
+$srcUri = "http://devsps01.blob.core.windows.net/base-os2012-ad/devsps01-devsps01-2013-12-05.vhd" 
  
-### Source Storage Account (West US) ###
+### Source Storage Account  ###
 $srcStorageAccount = "devsps01"
 $srcStorageKey = "t7WE/kfqL199HJVdbrZ82dySU6O9yq6hhAeNvYCM+nc45T3CwJyyo+HQByFxXxPaIEkriYEBc62IJQxGMzQLQg=="
  
-### Target Storage Account (West US) ###
+### Target Storage Account  ###
 $destStorageAccount = "meerkatmsdnspf"
 $destStorageKey = "W38cSAUwxN4Q7ujuAF30gYxsotDIR1N7uVab8GEZzC031RzxdS0xQHcEIUTRYG+ai+5NV5mFwcuME91sjlvmbg=="
  
@@ -24,11 +24,11 @@ $destContext = New-AzureStorageContext  –StorageAccountName $destStorageAccount 
 $containerName = "vhds"
  
 ### Create the container on the destination ### 
-get-AzureStorageContainer -Name $containerName -Context $destContext 
+New-AzureStorageContainer -Name $containerName -Context $destContext 
  
 ### Start the asynchronous copy - specify the source authentication with -SrcContext ### 
 $blob1 = Start-AzureStorageBlobCopy -srcUri $srcUri `
                                     -SrcContext $srcContext `
                                     -DestContainer $containerName `
-                                    -DestBlob "Drive2.vhd" `
+                                    -DestBlob "OS.vhd" `
                                     -DestContext $destContext
