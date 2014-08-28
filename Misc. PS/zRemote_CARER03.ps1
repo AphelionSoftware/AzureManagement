1ffe56cbdfe54e98657399c0b996c1763ea47634
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-28 18:34:26 +0000

    Remote

--- Misc. PS/zRemote_CARER03.ps1
+++ Misc. PS/zRemote_CARER03.ps1
@@ -8,10 +8,10 @@ Enter-PSSession -ComputerName carer03.cloudapp.net  -Credential (Get-Credential)
 
 Add-PSSnapin microsoft.sharepoint.powershell 
 
-$libURL = "http://carer03.cloudapp.net/Reports/ValueReports"
-$datasourcename = "rsdsPOA"
-$datasourceurl = "http://carer03.cloudapp.net/Reports/DataSources/MeerKat.rsds"
-$SpSite = "http://carer03.cloudapp.net/"
+#$libURL = "http://carer03.cloudapp.net/Reports/ValueReports"
+##$datasourcename = "rsdsPOA"
+#$datasourceurl = "http://carer03.cloudapp.net/Reports/DataSources/MeerKat.rsds"
+#$SpSite = "http://carer03.cloudapp.net/"
 
 #invoke-expression -Command "$ScriptPath\01_Meerkat.PSConfig.v001.ps1 -DatabaseServer $DatabaseServer -FarmAccount $FarmAccount"
 #invoke-expression -Command "$ScriptPath\02_Meerkat.Service Application Setup.v001.ps1 -DatabaseServer $DatabaseServer -FarmAccount $FarmAccount"
@@ -19,8 +19,8 @@ $SpSite = "http://carer03.cloudapp.net/"
 #invoke-expression -Command "$ScriptPath\04_Meerkat_ImportReportSite.ps1  -name $name -sitecoll $sitecoll -filePath $filePath -template $template -user $user"
 #invoke-expression -Command "$ScriptPath\06_FixLSiteReferences.ps1 -baseURL $baseURL -SpSite $SpSite"
 #invoke-expression -Command "$ScriptPath\07_SwitchMasterPage.ps1 -SpSite $SpSite -masterpage $masterpage"
-d:
-cd Dropbox\Github\Meerkat\installation\sp
-invoke-expression -Command ".\05_Meerkat_FixReportDataConnections.ps1 -webURL $SpSite  "
-exit
+#d:
+#cd Dropbox\Github\Meerkat\installation\sp
+#invoke-expression -Command ".\05_Meerkat_FixReportDataConnections.ps1 -webURL $SpSite  "
+#exit
 
